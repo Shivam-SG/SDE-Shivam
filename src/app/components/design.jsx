ef5770dc42a2d2a5d783727bd547c5fe68c16953
Shivam-SG <shivam70410@gmail.com>
2024-05-01 16:23:19 +0530

    done

--- src/app/components/design.jsx
+++ src/app/components/design.jsx
@@ -3,80 +3,80 @@ import React, { useState } from "react";
 import Image from "next/image";
 
 const Design = () => {
-  const initialContent = {
-    paragraph: (
-      <>
-        These problems, being so common, call for decisive and drastic
-        solutions. The solutions, below, however, only scratch the surface of
-        the deep underlying problems.
-      </>
-    ),
-    listItems: ["Low activation and adoption", "Low Retention", "Less churn"],
-  };
+  // const initialContent = {
+  //   paragraph: (
+  //     <>
+  //       These problems, being so common, call for decisive and drastic
+  //       solutions. The solutions, below, however, only scratch the surface of
+  //       the deep underlying problems.
+  //     </>
+  //   ),
+  //   listItems: ["Low activation and adoption", "Low Retention", "Less churn"],
+  // };
 
-  const [content, setContent] = useState(initialContent);
-  const [thirdSVGContent, setThirdSVGContent] = useState({});
-  const [isTextBlue, setIsTextBlue] = useState(false);
+  // const [content, setContent] = useState(initialContent);
+  // const [thirdSVGContent, setThirdSVGContent] = useState({});
+  // const [isTextBlue, setIsTextBlue] = useState(false);
 
-  const handleButtonClick = (svgNumber) => {
-    setIsTextBlue(true); // Set text color to blue when button is clicked
+  // const handleButtonClick = (svgNumber) => {
+  //   setIsTextBlue(true); // Set text color to blue when button is clicked
 
-    if (svgNumber === 1) {
-      setContent({
-        paragraph: (
-          <>
-            For most tech ventures, product performance is synonymous with
-            business performance. However, despite the vitality of the
-            product&apos;s success, the problems below couldn&apos;t be any more
-            common.
-          </>
-        ),
-        listItems: [
-          "Low activation and adoption",
-          "Low Retention",
-          "Less churn",
-        ],
-      });
-    } else if (svgNumber === 2) {
-      setContent({
-        paragraph: (
-          <>
-            These problems, being so common, call for decisive and drastic
-            solutions. The solutions, below, however, only scratch the surface
-            of the deep underlying problems.
-          </>
-        ),
-        listItems: [
-          "Hire more people",
-          "Launch new features",
-          "Increase the Marketing & Sales Budget",
-          "Retarget customers",
-        ],
-      });
-      setThirdSVGContent({}); // Reset third SVG content
-    } else if (svgNumber === 3) {
-      setThirdSVGContent({
-        paragraph: (
-          <>
-            Before reallocating resources to marketing or sales, ensure your
-            product&apos;s core is AI-enhanced for smarter performance. Our AI
-            solutions elevate your product by
-          </>
-        ),
-        listItems: [
-          "Predictive Analytics Enhancement",
-          "Personalized Experience Algorithms",
-          "Data-Informed Design Insights",
-          "Advanced Interaction Intelligence",
-          "Insight-Driven Feature Development",
-        ],
-      });
-    }
-  };
+  //   if (svgNumber === 1) {
+  //     setContent({
+  //       paragraph: (
+  //         <>
+  //           For most tech ventures, product performance is synonymous with
+  //           business performance. However, despite the vitality of the
+  //           product&apos;s success, the problems below couldn&apos;t be any more
+  //           common.
+  //         </>
+  //       ),
+  //       listItems: [
+  //         "Low activation and adoption",
+  //         "Low Retention",
+  //         "Less churn",
+  //       ],
+  //     });
+  //   } else if (svgNumber === 2) {
+  //     setContent({
+  //       paragraph: (
+  //         <>
+  //           These problems, being so common, call for decisive and drastic
+  //           solutions. The solutions, below, however, only scratch the surface
+  //           of the deep underlying problems.
+  //         </>
+  //       ),
+  //       listItems: [
+  //         "Hire more people",
+  //         "Launch new features",
+  //         "Increase the Marketing & Sales Budget",
+  //         "Retarget customers",
+  //       ],
+  //     });
+  //     setThirdSVGContent({}); // Reset third SVG content
+  //   } else if (svgNumber === 3) {
+  //     setThirdSVGContent({
+  //       paragraph: (
+  //         <>
+  //           Before reallocating resources to marketing or sales, ensure your
+  //           product&apos;s core is AI-enhanced for smarter performance. Our AI
+  //           solutions elevate your product by
+  //         </>
+  //       ),
+  //       listItems: [
+  //         "Predictive Analytics Enhancement",
+  //         "Personalized Experience Algorithms",
+  //         "Data-Informed Design Insights",
+  //         "Advanced Interaction Intelligence",
+  //         "Insight-Driven Feature Development",
+  //       ],
+  //     });
+  //   }
+  // };
 
-  const [isOpenChallenges, setIsOpenChallenges] = useState(false);
-  const [isOpenCompanies, setIsOpenCompanies] = useState(false);
-  const [isOpenWhatWeDo, setIsOpenWhatWeDo] = useState(false);
+  // const [isOpenChallenges, setIsOpenChallenges] = useState(false);
+  // const [isOpenCompanies, setIsOpenCompanies] = useState(false);
+  // const [isOpenWhatWeDo, setIsOpenWhatWeDo] = useState(false);
 
   return (
     <div className="p-4 sm:p-[100px] bg-black text-white pt-[120px]">
